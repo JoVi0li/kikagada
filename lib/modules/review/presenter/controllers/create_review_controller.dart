55d3b2e26ea0f9be7345048e2684ccec8ea493e3
Jovioli <jovioli.dev04@gmail.com>
2023-10-04 16:15:06 -0300

    feat: implement image pick action

--- lib/modules/review/presenter/controllers/create_review_controller.dart
+++ lib/modules/review/presenter/controllers/create_review_controller.dart
@@ -4,43 +4,65 @@ import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:kikagada/modules/review/domain/errors/review_errors.dart';
+import 'package:permission_handler/permission_handler.dart';
 
-class CreateReviewController {
-  CreateReviewController() {
-    initController();
-  }
-
+abstract interface class ICreateReviewController {
   late final TextEditingController titleController;
   late final TextEditingController bodyController;
   late List<File> photos;
+  void initController();
+  void disposeController();
+  Future<(List<File>? photos, ReviewError? error)> pickPhotos(
+    BuildContext context,
+  );
+}
 
+class CreateReviewController implements ICreateReviewController {
+  CreateReviewController();
+
+  @override
+  late final TextEditingController titleController;
+  @override
+  late final TextEditingController bodyController;
+  @override
+  late List<File> photos;
+
+  @override
   void initController() {
     titleController = TextEditingController();
     bodyController = TextEditingController();
     photos = [];
   }
 
+  @override
   void disposeController() {
     titleController.dispose();
     bodyController.dispose();
   }
 
+  @override
   Future<(List<File>? photos, ReviewError? error)> pickPhotos(
     BuildContext context,
   ) async {
+    while (await Permission.photos.status == PermissionStatus.denied) {
+      await askPermissionToAccessPhoto();
+    }
     try {
       final result = await FilePicker.platform.pickFiles(
         allowMultiple: true,
         type: FileType.image,
-        allowedExtensions: ['png', 'jpg', 'jpeg'],
       );
 
-      if (result?.count != null && result!.count > 3) {
+      if (result == null) {
+        return (photos, null);
+      }
+
+      if (result.count > 3) {
         return (
           null,
           GenericReviewError(
-            error: 'Número máximo de imagens excedido',
-            message: 'Selecione no máximo 3 imagens',
+            error: 'Selecione no máximo 3 imagens',
+            message: null,
           ),
         );
       }
@@ -49,7 +71,7 @@ class CreateReviewController {
         photos = [];
       }
 
-      for (int index = 0; index < result!.count; index++) {
+      for (int index = 0; index < result.count; index++) {
         photos.add(File(result.files[index].path!));
       }
 
@@ -58,10 +80,14 @@ class CreateReviewController {
       return (
         null,
         GenericReviewError(
-          error: 'Ops! Algo deu errado',
-          message: e.toString(),
+          error: e.toString(),
+          message: null,
         ),
       );
     }
+  }
+
+  Future<PermissionStatus> askPermissionToAccessPhoto() async {
+    return await Permission.photos.request();
   }
 }
--- lib/modules/review/presenter/screens/create_review_screen.dart
+++ lib/modules/review/presenter/screens/create_review_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:kikagada/modules/review/presenter/controllers/create_review_controller.dart';
 import 'package:kikagada/shared/components/app_bar_component.dart';
+import 'package:kikagada/shared/components/error_dialog_component.dart';
 
 class CreateReviewScreen extends StatefulWidget {
   const CreateReviewScreen({super.key});
@@ -9,6 +11,34 @@ class CreateReviewScreen extends StatefulWidget {
 }
 
 class _CreateReviewScreenState extends State<CreateReviewScreen> {
+  late final ICreateReviewController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = CreateReviewController();
+    _controller.initController();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.disposeController();
+  }
+
+  Future<void> pickImages(BuildContext context) async {
+    final (_, error) = await _controller.pickPhotos(context);
+
+    setState(() {});
+
+    if (error != null) {
+      showDialog(
+        context: context,
+        builder: (ctx) => ErrorDialogComponent(errorMessage: error.error),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,10 +50,67 @@ class _CreateReviewScreenState extends State<CreateReviewScreen> {
         ),
       ),
       body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16),
         child: Form(
           child: Column(
             children: [
-              TextFormField(),
+              TextFormField(
+                controller: _controller.titleController,
+                style: Theme.of(context)
+                    .textTheme
+                    .bodyLarge
+                    ?.copyWith(color: Colors.white),
+                maxLength: 50,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Título',
+                ),
+              ),
+              const SizedBox(height: 32),
+              TextFormField(
+                controller: _controller.bodyController,
+                style: Theme.of(context)
+                    .textTheme
+                    .bodyLarge
+                    ?.copyWith(color: Colors.white),
+                maxLines: 10,
+                maxLength: 1200,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Relato',
+                ),
+              ),
+              const SizedBox(height: 32),
+              TextButton(
+                onPressed: () async => await pickImages(context),
+                child: Row(
+                  children: [
+                    const Icon(
+                      Icons.upload,
+                      color: Colors.white,
+                    ),
+                    Text(
+                      'Subir fotos',
+                      style: Theme.of(context)
+                          .textTheme
+                          .bodyLarge
+                          ?.copyWith(color: Colors.white),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                width: MediaQuery.sizeOf(context).width,
+                height: 200,
+                child: ListView.builder(
+                  scrollDirection: Axis.vertical,
+                  itemCount: _controller.photos.length,
+                  itemBuilder: (ctx, index) {
+                    return ListTile(
+                        title: Text(_controller.photos[index].path));
+                  },
+                ),
+              ),
             ],
           ),
         ),
