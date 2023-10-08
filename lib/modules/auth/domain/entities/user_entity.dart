ca7f5ca6babbd4314f1eede31215b9881e20fd34
Jovioli <jovioli.dev04@gmail.com>
2023-10-08 17:12:14 -0300

    feat: implements new requirements to user entity

--- lib/modules/auth/domain/entities/user_entity.dart
+++ lib/modules/auth/domain/entities/user_entity.dart
@@ -9,7 +9,10 @@ class UserEntity {
     required this.name,
     required this.email,
     required this.photo,
-  });
+  })  : assert(id.isNotEmpty, 'id property can not be empty'),
+        assert(name.isNotEmpty, 'name property can not be empty'),
+        assert(email.isNotEmpty, 'email property can not be empty'),
+        assert(email.contains('@'), 'email property value is invalid');
 
   factory UserEntity.fromMap(Map<String, dynamic> map) {
     return UserEntity(
