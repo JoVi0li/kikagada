6f76b7359453e542da17c93fea4baf49bab998ff
Jovioli <jovioli.dev04@gmail.com>
2023-10-17 20:54:17 -0300

    refactor: finish review details screen refactor

--- lib/modules/review/presenter/widgets/review_details_widgets/error_review_details_widget.dart
+++ lib/modules/review/presenter/widgets/review_details_widgets/error_review_details_widget.dart
@@ -1,21 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:kikagada/modules/review/domain/errors/review_errors.dart';
+import 'package:kikagada/modules/review/presenter/screens/home_screen.dart';
+import 'package:kikagada/modules/review/presenter/widgets/review_details_widgets/review_details_loading_widget.dart';
+import 'package:kikagada/shared/components/button_component.dart';
+import 'package:kikagada/shared/components/dialog_component.dart';
 
 class ErrorReviewDetailsWidget extends StatelessWidget {
   const ErrorReviewDetailsWidget({super.key, required this.error});
   final ReviewError error;
 
+  void onPressed(BuildContext context) async {
+    return await Navigator.pushAndRemoveUntil<void>(
+      context,
+      MaterialPageRoute(builder: (ctx) => const HomeScreen()),
+      (route) => false,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Icon(Icons.error, size: 36, color: Colors.white),
-          const SizedBox(height: 12),
-          Text(error.error, style: Theme.of(context).textTheme.labelMedium),
-        ],
-      ),
-    );
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      return showAdaptiveDialog<void>(
+        context: context,
+        builder: (ctx) {
+          return DialogComponent(
+            title: 'Erro ao carregar a review',
+            content: error.error,
+            actions: [
+              ButtonComponent(
+                onPressed: () => onPressed(context),
+                label: 'Voltar para tela inicial',
+              ),
+            ],
+          );
+        },
+      );
+    });
+    return const ReviewDetailsLoadingWidget();
   }
 }
